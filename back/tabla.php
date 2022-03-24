c24a35522cab522ae1b00f68a9afd6758c0abdee
DESKTOP-R6ASI0J\Hidan <hidan.parra@gmail.com>
2022-03-24 18:06:36 -0500

    Se rediseña el navbar y la tabla

--- back/tabla.php
+++ back/tabla.php
@@ -9,26 +9,135 @@
     <title>Servicios</title>
 </head>
 <body>
+<div class="container-fluid padre">
+  <!--NAVBAR-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <h1 class="navbar-brand">SERVICIO</h1>
+    <!--CALENDARIO-->
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="date">
+    </form>
+    <!--BOTON TOGGLER-->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!--DIV DE CONTENIDO PARA EL TOGGLER-->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!--GRUPO DE BOTONES-->
+      <ul class="navbar-nav mr-auto">
+        <!--DROPDOWN DEL BOTON AGREGAR-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Agregar
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Categoria</a>
+            <a class="dropdown-item" href="#">Servicio</a>
+            <a class="dropdown-item" href="#">Material</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#"></a>
+          </div>
+        </li>
+        <!--FIN DEL DROPDOWN DEL BOTON AGREGAR-->
+        <!--DEMAS BOTONES-->
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cambiar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Borrar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cerrar Sesión</a>
+        </li>
+      </ul>
+      <!--FIN DEL GRUPO BOTONES-->
+    </div>
+    <!--FIN DEL CONTENIDO DEL TOGGLE-->
+  </nav>
+  <!--FIN DEL NAVBAR-->
+  <!--INICIO DEL TABLE-->
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">MATERIAL</th>
+      <th scope="col">CANTIDAD</th>
+      <th scope="col">DOMINGO</th>
+      <th scope="col">LUNES</th>
+      <th scope="col">MARTES</th>
+      <th scope="col">MIERCOLES</th>
+      <th scope="col">JUEVES</th>
+      <th scope="col">VIERNES</th>
+      <th scope="col">SABADO</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Material 1</td>
+      <td>Cantidad 1</td>
+      <td>Numero D</td>
+      <td>Numero L</td>
+      <td>Numero M</td>
+      <td>Numero M</td>
+      <td>Numero J</td>
+      <td>Numero V</td>
+      <td>Numero S</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Material 2</td>
+      <td>Cantidad 2</td>
+      <td>Numero D</td>
+      <td>Numero L</td>
+      <td>Numero M</td>
+      <td>Numero M</td>
+      <td>Numero J</td>
+      <td>Numero V</td>
+      <td>Numero S</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Material 3</td>
+      <td>Cantidad 3</td>
+      <td>Numero D</td>
+      <td>Numero L</td>
+      <td>Numero M</td>
+      <td>Numero M</td>
+      <td>Numero J</td>
+      <td>Numero V</td>
+      <td>Numero S</td>
+    </tr>
+  </tbody>
+</table>
+
+
+</div>
+
+
+
+<!--
+
   <div class="wrapper">
-<!--NAVBAR-->
+NAVBAR
   <div id="content">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <h1>Nombre Servicio</h1>
         <input type="date" name="" id="">
-        <button type="button" id="sidebarCollapse" class="btn btn-info">
+        <button type="button" id="" class="btn btn-info">
               <span>Agregar</span>
         </button>
-        <button type="button" id="sidebarCollapse" class="btn btn-info">
+        <button type="button" id="" class="btn btn-info">
               <span>Cambiar</span>
         </button>
-        <button type="button" id="sidebarCollapse" class="btn btn-info">
+        <button type="button" id="" class="btn btn-info">
               <span>Imprimir</span>
         </button>
-        <button type="button" id="sidebarCollapse" class="btn btn-danger">
+        <button type="button" id="" class="btn btn-danger">
               <span>Borrar</span>
         </button>
-        <button type="button" id="sidebarCollapse" class="btn btn-success">
+        <button type="button" id="" class="btn btn-success">
               <span>Cerrar Sesión</span>
         </button>
 
@@ -94,7 +203,7 @@
 
 
 </div>
-
+-->
     <script src="../js/main.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
